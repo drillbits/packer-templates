c97deb45a29339ba16abdb724e72d6e367c104c4
drillbits <neji@drillbits.jp>
2014-04-13 05:43:16 +0900

    Revert commits to develop
    * commit 921e2bf1cac08de2e3adee37440a386b6d4e94aa.
    * commit 0e931528c6bc4e8dec8ad65286495700b456cd63.

--- ubuntu-12.04/scripts/base.sh
+++ ubuntu-12.04/scripts/base.sh
@@ -6,6 +6,3 @@ sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
-
-sudo locale-gen ja_JP.UTF-8
-sudo dpkg-reconfigure locales
